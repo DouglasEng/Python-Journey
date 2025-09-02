27622ba59f4274fef865fcd6e8d1d2491b7a98dd
Douglas Rodrigues <douglasrodriguesgithub@gmail.com>
2025-09-02 16:30:15 -0300

    Dia 2 - Realizando

---
+++ Mes1/Dia2/main.py
@@ -0,0 +1,98 @@
+'''
+=== Gerenciador de Tarefas ===
+1 - Adicionar tarefa
+2 - Listar tarefas
+3 - Atualizar tarefa
+4 - Remover tarefa
+5 - Sair
+
+Escolha uma opção: 1
+Digite o título da tarefa: Estudar Python
+Tarefa adicionada com sucesso!
+
+Escolha uma opção: 2
+[1] Estudar Python - Pendente
+
+Escolha uma opção: 3
+Digite o ID da tarefa que deseja atualizar: 1
+Novo título (ou enter para manter): Estudar Python avançado
+Novo status (Pendente / Em andamento / Concluída): Em andamento
+Tarefa atualizada com sucesso!
+
+Escolha uma opção: 2
+[1] Estudar Python avançado - Em andamento
+
+
+
+'''
+
+import os
+from time import sleep
+
+status = ['Pendente', 'Realizando', 'Concluido']
+tarefas = []
+while True:
+    os.system('cls')
+    opcao = int(input('''=== Gerenciador de Tarefas ===
+1 - Adicionar tarefa
+2 - Listar tarefas
+3 - Atualizar tarefa
+4 - Remover tarefa
+5 - Sair\n
+    Digite o valor correspondente:  '''))
+    
+    os.system('cls')
+    if opcao == 1:
+        tarefa = str(input('Digite o titulo da tarefa: ').strip())
+        tarefas.append([tarefa, status[0]])
+    if opcao == 2:
+        for items in tarefas:
+            print(f'{items[0]}  -  {items[1]}')
+        
+        cont = input('Aperte enter para continuar.')
+
+    if opcao == 3:
+        for contador, items in enumerate(tarefas):
+            print(f'{contador+1} {items[0]}  -  {items[1]}')
+        opcaoRetirada = int(input('\nDigite a opção da tarefa: '))
+        
+        statusTarefaAtual = int(input('''
+1 - Pendente
+2 - Realizando
+3 - Concluido
+'''))
+
+        if tarefas[opcaoRetirada-1][1] == status[statusTarefaAtual-1]:
+            print('Não é possível alterar para um status semelhante.')
+        
+        elif statusTarefaAtual == 1:
+            tarefas[opcaoRetirada-1][1] = status[0]
+        elif statusTarefaAtual == 2:
+            tarefas[opcaoRetirada-1][1] = status[1]
+        elif statusTarefaAtual == 3:
+            tarefas[opcaoRetirada-1][1] = status[2]
+        else:
+            print('Valor invalido!')
+        cont = input('Aperte enter para continuar.')
+
+    if opcao == 4:
+        for contador, items in enumerate(tarefas):
+            print(f'{contador+1} {items[0]}  -  {items[1]}')
+        opcaoRetirada = int(input('\nDigite a opção da tarefa: '))
+
+
+        tarefas.pop(opcaoRetirada-1)
+    
+    if opcao == 5:
+        for contador in range(0,3):
+            for contador2 in range(0,3):
+                print("Carregando.", end='')
+                print('.'*contador2)
+                sleep(1)
+                os.system('cls')
+        print('Obrigado! Volte Sempre.')
+        break
+        
+
+
+        
